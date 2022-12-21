4fca79163e0481b2f67862301ca402b9e8ec3367
camilledeflesselle <camille.deflesselle@edu.esiee.fr>
2022-12-20 22:30:58 -0500

    Fix : cook before adding to orders

--- cafe-api/src/main/java/ca/ulaval/glo4002/cafe/application/cooking/CookingService.java
+++ cafe-api/src/main/java/ca/ulaval/glo4002/cafe/application/cooking/CookingService.java
@@ -2,12 +2,13 @@ package ca.ulaval.glo4002.cafe.application.cooking;
 
 import ca.ulaval.glo4002.cafe.domain.cooking.Cooker;
 import ca.ulaval.glo4002.cafe.domain.inventory.IInventoryRepository;
+import ca.ulaval.glo4002.cafe.domain.inventory.Inventory;
 import ca.ulaval.glo4002.cafe.domain.order.Order;
 import ca.ulaval.glo4002.cafe.domain.recipe.IRecipeRepository;
 
 public class CookingService {
-    private IRecipeRepository recipeRepository;
-    private IInventoryRepository inventoryRepository;
+    private final IRecipeRepository recipeRepository;
+    private final IInventoryRepository inventoryRepository;
 
     public CookingService(IRecipeRepository recipeRepository, IInventoryRepository inventoryRepository) {
         this.inventoryRepository = inventoryRepository;
@@ -15,7 +16,8 @@ public class CookingService {
     }
 
     public void cookOrder(Order newOrder) {
-        Cooker cooker = new Cooker(recipeRepository, inventoryRepository.getInventory());
+        Inventory inventory = this.inventoryRepository.getInventory();
+        Cooker cooker = new Cooker(recipeRepository, inventory);
         cooker.checkIfEnoughIngredientsForOrder(newOrder);
         cooker.cook(newOrder);
         inventoryRepository.save(cooker.getInventory());
